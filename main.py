98ceb90a097dc82e49f3e9d2b00c38bfac80ece0
terraboi4 <jaxstar24@gmail.com>
2026-04-03 12:14:43 -0500

    Start calculating angles

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from screeninfo import get_monitors
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-
+import math
 
 CONNECTIONS = [(0, 1), (1, 2), (2, 3), (3, 7), (0, 4), (4, 5),
                (5, 6), (6, 8), (9, 10), (11, 12), (11, 13),
@@ -12,6 +12,42 @@ CONNECTIONS = [(0, 1), (1, 2), (2, 3), (3, 7), (0, 4), (4, 5),
                (18, 20), (11, 23), (12, 24), (23, 24), (23, 25),
                (24, 26), (25, 27), (26, 28), (27, 29), (28, 30),
                (29, 31), (30, 32), (27, 31), (28, 32)]
+POINT_LANDMARKS = [
+    "nose",
+    "left_eye_inner",
+    "left_eye",
+    "left_eye_outer",
+    "right_eye_inner",
+    "right_eye",
+    "right_eye_outer",
+    "left_ear",
+    "right_ear",
+    "mouth_left",
+    "mouth_right",
+    "left_shoulder",
+    "right_shoulder",
+    "left_elbow",
+    "right_elbow",
+    "left_wrist",
+    "right_wrist",
+    "left_pinky",
+    "right_pinky",
+    "left_index",
+    "right_index",
+    "left_thumb",
+    "right_thumb",
+    "left_hip",
+    "right_hip",
+    "left_knee",
+    "right_knee",
+    "left_ankle",
+    "right_ankle",
+    "left_heel",
+    "right_heel",
+    "left_foot_index",
+    "right_foot_index",
+]
+visible_side = None
 
 BaseOptions = mp.tasks.BaseOptions
 PoseLandmarker = mp.tasks.vision.PoseLandmarker
@@ -41,21 +77,37 @@ if not cap.isOpened():
     print("Error: could not open video")
     exit()
 
+def angle_between(a, b, c):
+    # Angle between triangle ABC where A, B, and C are indexes of points on the body
+    ab = math.dist(a, b)
+    bc = math.dist(b, c)
+    ca = math.dist(c, a)
+    return math.acos((ab**2+bc**2-ca**2)/(2*ab*bc))*(180/math.pi)
+
+
 
 def draw_lines(result, frame):
-    points = []
+    global visible_side
+
+    points = {}
     height, width = frame.shape[:2]
 
-    for i, lm in enumerate(result.pose_landmarks[0]):
-        x, y = int(lm.x*width), int(lm.y*height)
-        points.append((x, y))
-        cv2.circle(frame, (x, y), 5, (255, 255, 255))
-        # cv2.putText(frame, str(CONNECTIONS[i][1]-1), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
-    for (start, end) in CONNECTIONS:
-        if start < len(points) and end < len(points):
-            cv2.line(frame, points[start],
-                     points[end], (255, 255, 255), 2)
+    if len(result.pose_landmarks) > 0:
+        
+        for i, lm in enumerate(result.pose_landmarks[0]):
+            
+            x, y = int(lm.x*width), int(lm.y*height)
+            points[POINT_LANDMARKS[i]] = (x,y)
+            cv2.circle(frame, (x, y), 5, (255, 255, 255))
+            cv2.putText(frame, str(i), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            
+        for (start, end) in CONNECTIONS:
+            if start < len(points) and end < len(points):
+                cv2.line(frame, points.keys()[start],
+                        points.keys()[end], (255, 255, 255), 2)
+        if visible_side is None:
+            visible_side = min(points["left_hip"].z, points["right_hip"].z)
+        cv2.putText(frame, str(visible_side), (100,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
 
 while True:
